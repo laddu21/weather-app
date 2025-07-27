c7ba8deb68cd3cc80c09fa56fe2f8e119cd57200
Rabbani <rabbanisk162002@gmail.com>
2025-07-27 10:27:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import Weather from './Weather'; // Importing the Weather component
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+    </div>
+  );
+}
+
+export default App;
